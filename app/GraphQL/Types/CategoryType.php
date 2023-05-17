0094d5f1ddc5edccbb7aa17e9885dad00074f2d9
Tanvir Hossen Bappy <tanvirbappy7706@gmail.com>
2023-05-17 16:57:32 +0600

    type int

--- app/GraphQL/Types/CategoryType.php
+++ app/GraphQL/Types/CategoryType.php
@@ -2,7 +2,34 @@
 
 namespace App\GraphQL\Types;
 
-class CategoryType
-{
+use App\Models\Category;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Type as GraphQlType;
 
+class CategoryType extends GraphQlType
+{
+    protected $attributes = [
+        'name' => 'Category',
+        'description' => 'Collection of categories',
+        'model' => Category::class
+    ];
+
+    public function fields(): array
+    {
+        return [
+            'id' => [
+                'type' => Type::nonNull(Type::int()),
+                'description' => 'ID of category'
+            ],
+            'title' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'Title of category'
+            ],
+            'quest' => [
+                'type' => Type::listOf(GraphQL::type('Quest')),
+                'description' => 'List of quests'
+            ]
+        ];
+    }
 }
--- app/GraphQL/Types/QuestType.php
+++ app/GraphQL/Types/QuestType.php
@@ -2,7 +2,42 @@
 
 namespace App\GraphQL\Types;
 
-class QuestType
-{
+use App\Models\Quest;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Type as GraphQlType;
 
+class QuestType extends GraphQlType
+{
+    protected $attributes = [
+        'name' => 'Quest',
+        'description' => 'Collection of quests with their respective category',
+        'model' => Quest::class
+    ];
+
+    public function fields(): array
+    {
+        return [
+            'id' => [
+                'type' => Type::nonNull(Type::int()),
+                'description' => 'ID of quest'
+            ],
+            'title' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'Title of quest'
+            ],
+            'description' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'Description of quest'
+            ],
+            'reward' => [
+                'type' => Type::nonNull(Type::int()),
+                'description' => 'Reward of quest'
+            ],
+            'category' => [
+                'type' => GraphQL::type('Category'),
+                'description' => 'The category of the quest'
+            ],
+        ];
+    }
 }
